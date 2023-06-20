5567db1dbe0ed9a2c88cb4186e4e083bf8b0d25b
Alena <alenabelovadmvn@gmail.com>
2023-06-20 16:13:00 -0400

    Initial Commit

---
+++ Xcode Demo/Xcode_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_DemoApp.swift
+//  Xcode Demo
+//
+//  Created by Alena Belova  on 2023-06-20.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
