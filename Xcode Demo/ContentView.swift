8ff98d5fe9847a012365402ba32b5a6f08b71f61
Alena <alenabelovadmvn@gmail.com>
2023-06-27 15:45:55 -0400

    background and shadow added

--- Xcode Demo/ContentView.swift
+++ Xcode Demo/ContentView.swift
@@ -9,15 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image("land")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
+        
+        ZStack {
+            VStack(alignment: .leading, spacing: 20.0 ) {
+                Image("land")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                HStack(alignment: .center) {
+                    Text("Landscape")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    VStack{
+                        HStack{
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                        }
+                        Text("(Reviews 361)")
+                    }
+                    .foregroundColor(.orange)
+                    .font(.caption)
+                }
+                
+                Text("Come visit us")
+                HStack{
+                    Spacer()
+                    Image(systemName: "binoculars.fill")
+                    Image(systemName: "fork.knife")
+                }
+                .foregroundColor(.gray)
+                .font(.caption)
+            }
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.white)
                 .cornerRadius(15)
-            
-            Text("Landscape")
-            Text("Come visit us")
+                .shadow(radius: 15))
+            .padding()
         }
+        
+        
     }
 }
 
